5e07db3b7b5af73f7ad7df77716aa27c3ac6a66d
Jason Brumwell <jason.brumwell@gmail.com>
2017-07-31 14:58:30 -0400

    Support Axios

---
+++ src/transports/axios.js
@@ -0,0 +1,62 @@
+/* global window */
+import Promise from 'bluebird';
+import _ from 'lodash';
+import qs from 'qs';
+import Response from './response';
+
+let axiosSupported = true;
+
+try {
+  const axios = require('axios');
+} catch (e) {
+  axiosSupported = false;
+}
+
+/**
+ * Make an api request
+ *
+ * @param  {Object} options
+ * @param  {Function} callback
+ * @return {Promise}
+ */
+export default (config, opts) => {
+  return Promise.try(() => {
+    if (! axiosSupported) {
+      throw new Error('Axios must be installed to use the fetch transport');
+    }
+
+    if (! _.isPlainObject(opts) || _.keys(opts).length === 0) {
+      throw new Error('Options must be a non-empty object');
+    }
+
+    const isPlainObject = _.isPlainObject(opts.payload);
+
+    opts.method = opts.method ? opts.method.toLowerCase() : 'get';
+
+    if (['get', 'head'].indexOf(opts.method) === -1) {
+      if (isPlainObject) {
+        opts.data = opts.payload;
+        _.set(opts, 'headers.Content-Type', 'application/json');
+      } else if (opts.payload) {
+        opts.data = opts.payload;
+      }
+    } else if (isPlainObject) {
+      opts.params = opts.payload;
+    }
+
+    return axios({
+      url : opts.url,
+      method : opts.method,
+      headers : opts.headers,
+      data : opts.data,
+      params : opts.params,
+      withCredentials : !! opts.credentials,
+    }).then((res) => {
+      return new Response(res.data, {
+        statusCode : res.status,
+        headers : res.headers,
+        url : opts.url,
+      })
+    });
+  });
+};
--- src/transports/index.js
+++ src/transports/index.js
@@ -1,7 +1,9 @@
 import hapi from './hapi';
 import fetch from './fetch';
+import axios from './axios';
 
 export default {
+  axios,
   hapi,
   fetch,
 };
